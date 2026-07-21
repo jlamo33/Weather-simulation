1e8285c48f6a221e9a58d6d4907e374de417f907
jlamo33 <jlamori@wgu.edu>
2026-07-20 22:09:39 -0400

    Initial Commit

---
+++ UpdatedSim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  UpdatedSim
+//
+//  Created by Joshua LaMoria on 7/20/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}

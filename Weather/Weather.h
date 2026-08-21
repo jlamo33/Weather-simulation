0ae59351d6349135788550b12c8cccb09e4f56d0
jlamo33 <jlamori@wgu.edu>
2026-08-21 10:50:46 -0400

    ADDED weather class and weather structs.

---
+++ Weather/Weather.h
@@ -0,0 +1,72 @@
+//
+//  Weather.h
+//  UpdatedSim
+//
+//  Created by Joshua LaMoria on 8/20/26.
+//
+
+enum weatherState {
+    
+    RAIN,
+    CLEAR,
+    THUNDERSTORM,
+    CLOUDY
+    
+};
+
+
+struct Rain {
+    
+    
+
+struct Snow {
+    
+    
+}
+
+struct Thunder {
+    
+    
+}
+
+struct Clouds {
+    
+    
+}
+
+struct Temperature {
+    
+    
+}
+
+struct ClearWeather {
+    
+    
+}
+
+struct wind {
+    
+    
+    
+}
+
+struct naturalDiaster {
+    
+    
+}
+
+class Weather {
+    
+    
+private:
+    
+    private int weatherChangeInterval;
+    
+    
+    
+public:
+    
+    int getWeatherChangeInterval();
+    void setweatherChangeInterval(int newChangeInterval);
+};
+
